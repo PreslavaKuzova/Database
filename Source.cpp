676b57b229e2ed54e9ae4b1587c247ac38ae19ac
PreslavaKuzova <preslava.kuzova@gmail.com>
2019-06-19 15:03:47 +0800

    Column modification

--- Source.cpp
+++ Source.cpp
@@ -4,17 +4,33 @@
 #include "Database.h"
 #include "ColumnController.h"
 #include "TableController.h"
+#include "DatabaseController.h"
 
 int main() {
 	//have to use the copy constructor
-	Column column(ColumnController().createColumn("name", "incorrect type", true));
+	Column column(ColumnController().createColumn("name", "incorrect type"));
 	std::cout << ColumnController().getColumnInfo(column);
 
 	Table table(TableController().createTable("!awesome table!"));
 	TableController().describe(table);
-	TableController().addColumn(table, "new column", "INT");
+	TableController().createColumn(table, "new column", "INT");
 	TableController().describe(table);
 	TableController().columns(table);
+	TableController().addColumn(table, column);
+	TableController().columns(table);
+	TableController().describe(table);
 
+	Database database("new database");
+	DatabaseController().create(database, "table1");
+	DatabaseController().create(database, "table2");
+	DatabaseController().create(database, "table3");
+	DatabaseController().tableNames(database);
+	std::cout << std::endl << "Table exists: " << DatabaseController().tableExists(database, "tale1");
+	std::cout << "\nRename: "<< std::endl;
+	DatabaseController().renameTable(database, "table1", "AWESOME TABLE");
+	DatabaseController().tableNames(database);
+	DatabaseController().remove(database, "table2");
+	DatabaseController().tableNames(database);
+	DatabaseController().create(database, "table3");
 	return 0;
 }
