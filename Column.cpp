28e9e8215d852e935f1085fab82502bb9d79b94b
PreslavaKuzova <preslava.kuzova@gmail.com>
2019-06-19 09:29:41 +0800

    Add column implementation

---
+++ Column.cpp
@@ -0,0 +1,33 @@
+#include "Column.h"
+
+Column::Column(): 
+	columnName(""), type("int"), unique(false) {
+}
+
+Column::Column(std::string name, std::string type, bool unique): 
+	columnName(name), type(type), unique(unique) {
+}
+
+std::string Column::getColumnName() const {
+	return this->columnName;
+}
+
+void Column::setColumnName(std::string name) {
+	this->columnName = name;
+}
+
+std::string Column::getType() const {
+	return this->type;
+}
+
+void Column::setType(std::string type) {
+	this->type = type;
+}
+
+bool Column::getUnique() const {
+	return this->unique;
+}
+
+void Column::setUnique(bool unique) {
+	this->unique = unique;
+}
